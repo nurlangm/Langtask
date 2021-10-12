3f11acb9725ecd859a1a6a384dbfed3969eb54ce
unknown <you@example.com>
2021-10-12 11:53:02 +0400

    Task

---
+++ TASKKK/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList sortedlist = new SortedList();
+
+            sortedlist.Add("Csharp", 2000);
+            sortedlist.Add("Cplusplus", 1983);
+            sortedlist.Add("Python", 1991);
+            sortedlist.Add("Css", 1996);
+
+
+            foreach (DictionaryEntry item in sortedlist)
+            {
+                Console.WriteLine(item.Key);
+            }
+            Console.WriteLine("**");
+            foreach (DictionaryEntry item in sortedlist)
+
+            {
+
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
